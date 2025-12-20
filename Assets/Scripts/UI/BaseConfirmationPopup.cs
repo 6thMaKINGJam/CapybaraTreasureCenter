d01a6eb355f54fe4c19d664ea4dcb2d0616ce440
uri-git23 <saintname31@gmail.com>
2025-12-20 23:14:39 +0900

    fix: 타이포 수정

--- Assets/Scripts/UI/BaseConfirmationPopup.cs
+++ Assets/Scripts/UI/BaseConfirmationPopup.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Text, Button 기능
+using System; // Action(콜백) 기능
 
-public class BaseConfirmationPop : MonoBehaviour
+public class BaseConfirmationPopup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("UI 요소 연결")]
+    public Text MessageText; // 팝업에 표시될 질문 내용
+    public Button YesButton; // Yes 버튼
+    public Button NoButton; // No 버튼
+
+    // 버튼을 눌렀을 때 실행될 Action들
+    private Action OnYesAction;
+    private Action OnNoAction;
+
+    // 팝업 설정하고 화면에 표시
+    public void Setup(string Message, Action YesCallback, Action NoCallback)
     {
-        
+        MessageText.text = Message;
+        OnYesAction = YesCallback;
+        OnNoAction = NoCallback;
+
+        // 버튼 클릭 이벤트 연결
+        YesButton.onClick.RemoveAllListeners(); // 이전 연결 삭제 (중복 방지)
+        YesButton.onClick.AddListener(OnClickYes);
+
+        NoButton.onClick.RemoveAllListeners();
+        NoButton.onClick.AddListener(OnClickNo);
     }
 
-    // Update is called once per frame
-    void Update()
+    // OnClickYes 정의
+    private void OnClickYes()
     {
-        
+        OnYesAction?.Invoke(); // 예약된 Yes Action 실행
+        ClosePopup();
+    }
+
+    // OnClickNo 정의
+    private void OnClickNo()
+    {
+        OnNoAction?.Invoke(); // 예약된 No Action 실행
+        ClosePopup();
+    }
+
+    // ClosePopup 정의
+    private void ClosePopup()
+    {
+        Destroy(gameObject); // 팝업 비활성화
     }
 }
