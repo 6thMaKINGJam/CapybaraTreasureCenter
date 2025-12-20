1512d4dbf05329835fede853981f54eb0b7eb6a9
z44446in <z44446in@gmail.com>
2025-12-21 00:31:06 +0900

    기타 연결 설정 완료

--- Assets/Scripts/Manager/RankingManager.cs
+++ Assets/Scripts/Manager/RankingManager.cs
@@ -104,7 +104,7 @@ public class RankingManager : MonoBehaviour
             {
                 // 업로드 성공 시 ProgressData 업데이트
                 var data = SaveManager.LoadData<ProgressData>("ProgressData");
-                data.hasSeenLevel4Ending = true; // endingCompleted 대응
+                data.EndingCompleted = true; // endingCompleted 대응
                 SaveManager.Save(data, "ProgressData");
                 
                 onSuccess?.Invoke();
